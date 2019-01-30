9f47db8dab0ecd3fa58a5e2a9fd6305648d93700
Kenneth M Doud <kmdoud@gmail.com>
2019-01-30 16:43:10 -0500

    Initial Commit

---
+++ BankingProject/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProject
+{
+    class Account
+    {
+        private int Id { get; set; }
+        private string Description { get; set; }
+        private double Balance { get; set; }
+
+        public string GetDescription()
+        {
+            return Description;
+             }
+    public int GetId()
+        {
+            return Id;
+        }
+        public void SetDescription(string NewDescription)
+        {
+            Description = NewDescription;
+        }
+        public double GetBalance()
+        {
+            return Balance;
+        }
+        public void Deposit(double Amount)
+        {
+            Balance += Amount; 
+
+        }
+        public void Withdraw(double Amount)
+        {
+            Balance -= Amount;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Id ={Id}, Description = {Description}, and Balance = {Balance}");
+        }
+        public Account(string NewDescription)
+        {
+            Id = -1;
+            Description = NewDescription;
+            Balance = 0;
+        }
+        public Account()
+        {
+            Id = -1;
+            Description = "$New Account";
+            Balance = 0;
+
+        }
+
+    }
+}
--- BankingProject/Program.cs
+++ BankingProject/Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace BankingProject
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            var acct1 = new Account(("Test Checking"));
+            acct1.Print();
+            acct1.Deposit(1000.00);
+            acct1.Print();
+            acct1.Withdraw(60.00);
+            acct1.Print();
         }
     }
 }
